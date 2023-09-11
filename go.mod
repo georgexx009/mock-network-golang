041b63d98b2f8d5a4ff62db76308023730d75f35
Emmanuel <georgexx009@hotmail.com>
2023-09-11 14:13:22 -0600

    wip

---
+++ go.mod
@@ -0,0 +1,3 @@
+module mock-network-golang
+
+go 1.21.0
--- network/network.go
+++ network/network.go
@@ -1,7 +1,40 @@
 package network
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 func Init() {
-	fmt.Print("init network v0.0.1")
+	fmt.Print("init network v0.0.1 local 2")
+}
+
+func New() *Network {
+	return &Network{registeredNodes: make(map[string]Node)}
+}
+
+type Node interface {
+	receiveRequest(url string, httpMethod string, body string, headers string) (*Response, error)
+}
+
+type Network struct {
+	registeredNodes map[string]Node
+}
+
+func (network *Network) RegisterNode(url string, n Node) {
+	network.registeredNodes[url] = n
+}
+
+type Response struct {
+	ok         bool
+	body       string
+	statusCode int
+}
+
+func (network *Network) Send(url string, httpMethod string, body string, headers string) (*Response, error) {
+	if _, ok := network.registeredNodes[url]; !ok {
+		return nil, errors.New("url not found")
+	}
+
+	return network.registeredNodes[url].receiveRequest(url, httpMethod, body, headers)
 }
